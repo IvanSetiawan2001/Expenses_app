4a6a0dbace7bf5f5bf5adddcf837b731768afc1a
Ivan Setiawan <fanset.raharjo@gmail.com>
2022-11-14 10:42:56 +0900

    add $ sign

--- lib/Price.dart
+++ lib/Price.dart
@@ -17,12 +17,13 @@ class Price extends StatelessWidget {
         style: BorderStyle.solid,
       )),
       child: Text(
+        '\$' + amount.toString(),
+        // '\$ ${amount.toString()}' (this is another way to call the amount $ mean interpolaring some value so you can call the variable inside a string)
         style: TextStyle(
           color: Colors.purple,
           fontWeight: FontWeight.bold,
           fontSize: 20,
         ),
-        amount.toString(),
       ),
     );
   }
